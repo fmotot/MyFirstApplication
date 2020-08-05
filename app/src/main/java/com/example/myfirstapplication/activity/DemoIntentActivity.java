57206d666dcd7e6ae60e3fc81127a5805b7a914e
Frédérick <fmotot@gmail.com>
2020-08-05 10:59:06 +0200

    First intent to change activity

---
+++ app/src/main/java/com/example/myfirstapplication/activity/DemoIntentActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoIntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_intent);
+    }
+}
